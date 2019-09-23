9ad476939268e52ca5ce3f104bb23baf871382c7
chamith96 <chamithsandagiri@gmail.com>
2019-09-23 11:29:33 +0530

    add hystrix dashboard

--- movie-catelog-service/src/main/java/com/spring/moviecatelogservice/MovieCatelogServiceApplication.java
+++ movie-catelog-service/src/main/java/com/spring/moviecatelogservice/MovieCatelogServiceApplication.java
@@ -3,9 +3,11 @@ package com.spring.moviecatelogservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MovieCatelogServiceApplication {
 
 	public static void main(String[] args) {
